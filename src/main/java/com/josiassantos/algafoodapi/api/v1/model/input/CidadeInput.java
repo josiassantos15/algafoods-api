567081c6e6321d499e9ee709de90516bce7b9683
Josiassantos1577 <josiassantos1577@gmail.com>
2024-08-17 18:13:35 -0300

    feat: Criando servico para gerenciamento de Cidade

---
+++ src/main/java/com/josiassantos/algafoodapi/api/v1/model/input/CidadeInput.java
@@ -0,0 +1,22 @@
+package com.josiassantos.algafoodapi.api.v1.model.input;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class CidadeInput {
+
+	@Schema(example = "Uberlândia")
+	@NotBlank
+	private String nome;
+
+	@Valid
+	@NotNull
+	private EstadoIdInput estado;
+	
+}
